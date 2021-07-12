a0ada8533b827dabe61ff293c6c2e28c0a44c90b
daeyab <daeyab@naver.com>
2021-07-12 18:36:28 +0900

    Fix remove_emoji by importing emoji and add cleaning conditions

--- read_input.py
+++ read_input.py
@@ -2,6 +2,7 @@ import os
 from pathlib import Path
 import pymysql
 import re
+import emoji
 from datetime import datetime
 
 DATABASE_NAME = "chats"
@@ -288,36 +289,31 @@ def insert_into_monthly_chat_table(conn, table_name, datetime, sender, message):
     # message = message.strip()
     # print(message, end=" ")
     message = clean_text(message)
+    if len(message) > 0:
+        sql = (
+            "INSERT IGNORE INTO %s(date, sender, message) VALUES ('%s', '%s', '%s');"
+            % (
+                table_name,
+                datetime,
+                sender,
+                message,
+            )
+        )
+        conn.cursor().execute(sql)
+        conn.commit()
 
-    sql = "INSERT IGNORE INTO %s(date, sender, message) VALUES ('%s', '%s', '%s');" % (
-        table_name,
-        datetime,
-        sender,
-        message,
-    )
-    conn.cursor().execute(sql)
-    conn.commit()
 
-
-def remove_emoji(string):
-    emoji_pattern = re.compile(
-        "["
-        u"\U0001F600-\U0001F64F"  # emoticons
-        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-        u"\U0001F680-\U0001F6FF"  # transport & map symbols
-        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
-        u"\U00002702-\U000027B0"
-        u"\U000024C2-\U0001F251"
-        "]+",
-        flags=re.UNICODE,
-    )
-    return emoji_pattern.sub(r"", string)
+def remove_emoji(text):
+    return emoji.get_emoji_regexp().sub(u"", text)
 
 
 def clean_text(str):
     res = re.sub("[-=+,#/\?:^$.@*\"※~&%ㆍ!』\‘|\(\)\[\]\<\>`'…》]", "", str).replace(
         "\\", ""
     )
+    ls = ["Video", "Voice Memo", "Photo", "Emoticons"]
+    for l in ls:
+        res = res.replace(l, "")
     res = remove_emoji(res)
     return res
 
