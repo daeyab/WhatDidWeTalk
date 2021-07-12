6cab3c6b36609722b0f83af430c47d9a0389aa09
daeyab <daeyab@naver.com>
2021-07-12 18:17:03 +0900

    Implement reomve emoji

--- read_input.py
+++ read_input.py
@@ -4,7 +4,7 @@ import pymysql
 import re
 from datetime import datetime
 
-DATABASE_NAME = "chatings"
+DATABASE_NAME = "chats"
 HOST_NAME = "localhost"
 USER_ID = "root"
 USER_PASSWORD = "rootPW1!"
@@ -299,10 +299,26 @@ def insert_into_monthly_chat_table(conn, table_name, datetime, sender, message):
     conn.commit()
 
 
+def remove_emoji(string):
+    emoji_pattern = re.compile(
+        "["
+        u"\U0001F600-\U0001F64F"  # emoticons
+        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+        u"\U00002702-\U000027B0"
+        u"\U000024C2-\U0001F251"
+        "]+",
+        flags=re.UNICODE,
+    )
+    return emoji_pattern.sub(r"", string)
+
+
 def clean_text(str):
     res = re.sub("[-=+,#/\?:^$.@*\"※~&%ㆍ!』\‘|\(\)\[\]\<\>`'…》]", "", str).replace(
         "\\", ""
     )
+    res = remove_emoji(res)
     return res
 
 
